f1a557fd95b499714fb28c716912730348313e40
Roy Ben Yosef <60175164+royby-cyberark@users.noreply.github.com>
2023-05-02 10:09:47 +0300

    Update create_lambda_package.sh

--- create_lambda_package.sh
+++ create_lambda_package.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # clean the lambda function package directory
-rm -rf package
+# Maybe don't run destructive commands in this script? maybe better they fail if its there?
+# rm -rf package
 mkdir -p package
 
-# copy function code and install pacja
+# copy function code and install packages
 cp ./lambda_function.py package
 pip install python-jose requests --target package
 
 # temporary copy last boto3 version that supports Amazon Verified Permissions
+# You have a reference to your user folder...?
 cp -r /Users/Gil.Adda/dev/policy-decision-service/boto3 package
+# You have a reference to your user folder...?
 cp -r /Users/Gil.Adda/dev/policy-decision-service/botocore package
 
 # package the authorizer and packages to a zip file
@@ -20,3 +23,5 @@ popd
 # upload the package to the lambda function
 aws lambda update-function-code --function-name  avp_authorizer \
   --zip-file fileb://lambda_authorizer_package.zip
+
+# What about windows users?
