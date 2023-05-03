f6c28beacb931ae2393ff0fabb41c6cd71aea032
gadda <gil.adda@cyberark.com>
2023-05-03 11:41:24 +0300

    first draft of demo project

--- prepare_authorizer_package.sh
+++ prepare_authorizer_package.sh
@@ -6,5 +6,4 @@ cp ./lambda_function.py package
 pip install python-jose requests --target package
 # temporary copy last boto3 version that supports Amazon Verified Permissions
 # this will be deleted when boto3 will support Amazon Verified Permissions
-cp -r boto3 package
-cp -r botocore package
+unzip boto.zip -d package
