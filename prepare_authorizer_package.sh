4df07162d8ca048a9ad16bd5024b3856a1730677
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-05-11 10:26:33 -0500

    Using the safe version of requests

--- prepare_authorizer_package.sh
+++ prepare_authorizer_package.sh
@@ -3,7 +3,7 @@ set -e
 # prepare package to deploy: copy function code and install packages
 mkdir -p package
 cp ./lambda_function.py package
-pip install python-jose requests --target package
+pip install python-jose requests==2.29.0 --target package
 # temporary copy last boto3 version that supports Amazon Verified Permissions
 # this will be deleted when boto3 will support Amazon Verified Permissions
 unzip boto.zip -d package
