042f932207741298af447d10ec3cdc2846bb8307
gadda <gil.adda@cyberark.com>
2023-05-16 21:10:54 +0300

    Command line validation of paramaters

--- access-demo-resource.py
+++ access-demo-resource.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import argparse
-import json
-
 import requests
+from requests_oauth2client import OAuth2Client
 
 HTTP_VERY_LONG_TIMEOUT = 300
 identity_headers = {'Content-Type': 'application/json', 'X-IDAP-NATIVE-CLIENT': 'true'}
@@ -11,27 +10,13 @@ identity_headers = {'Content-Type': 'application/json', 'X-IDAP-NATIVE-CLIENT': 
 def identity_login(identity_url: str, username:str, password: str) ->str:
     try:
 
-        # 1st Phase - get user challenge (maybe password, password etc)
-        response = requests.api.post(f'{identity_url}/Security/StartAuthentication', json={
-            'User': username,
-            'Version': '1.0',
-            'PlatformTokenResponse': True
-        }, headers= identity_headers, timeout=HTTP_VERY_LONG_TIMEOUT)
-        auth_response = json.loads(response.text)
-        mechanism_id = auth_response['Result']['Challenges'][0]['Mechanisms'][0]['MechanismId']
-        session_id = auth_response['Result']['SessionId']
-
-        # 2nd Phase - respond with password
-        response = requests.api.post(
-            f'{identity_url}/Security/AdvanceAuthentication', json={
-                'SessionId': session_id,
-                'MechanismId': mechanism_id,
-                'Action': 'Answer',
-                'Answer': password
-            }, headers=identity_headers, timeout=HTTP_VERY_LONG_TIMEOUT)
-        advanced_auth_result = json.loads(response.text)
-        token = advanced_auth_result['Result']['Token']
+        oauth2client = OAuth2Client(
+            token_endpoint=f"{identity_url}/oauth2/platformtoken",
+            auth=(username, password),
+        )
+        token = oauth2client.client_credentials(scope="", resource="")
         return token
+
     except (Exception) as ex:
         print(ex)
 
@@ -51,8 +36,7 @@ def main():
     url =  f'{args.gw_url}/protected-resource'
     headers = { 'Authorization' : f'Bearer {token}' }
     response = requests.api.post(url,json={}, headers=headers)
-    print(response)
-
+    print(response.text)
 
 if __name__ == "__main__":
     main()
