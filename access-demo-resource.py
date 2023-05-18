21534e5c303bfbcbd8f6a9256a4aaae001abd78a
gadda <gil.adda@cyberark.com>
2023-05-18 17:16:02 +0300

    added python jose to the installs

--- access-demo-resource.py
+++ access-demo-resource.py
@@ -4,9 +4,6 @@ import argparse
 import requests
 from requests_oauth2client import OAuth2Client
 
-from lambda_function import _get_user_attributes, verify_oidc_token_signature, _get_data_entities
-from package.jose import jwt
-
 HTTP_VERY_LONG_TIMEOUT = 300
 identity_headers = {'Content-Type': 'application/json', 'X-IDAP-NATIVE-CLIENT': 'true'}
 
@@ -34,13 +31,6 @@ def main():
     # login and get token
     token = identity_login(username=args.user, password=args.password, identity_url=args.identity_url)
     print(f'token: {token}')
-    # get user attributes
-    verify_oidc_token_signature(tenant_url=args.identity_url, token=token)
-    claims = jwt.get_unverified_claims(token)
-    user_id = claims['sub']
-    print(f'principal: {user_id}')
-    user_attributes = _get_user_attributes(tenant_url=args.identity_url, token=token, user_id=user_id)
-    print(f'user attributes: {user_attributes}')
 
     # call api gateway resource, protected by token authorizer and Amazon Verified Permissions as the decision service
     url = f'{args.gw_url}/protected-resource'
